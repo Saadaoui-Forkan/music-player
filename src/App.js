eb1609977a31bb8a24c430f0d903b9a626657119
unknown <forkan.saadaoui@gmail.com>
2023-04-12 02:37:37 +0100

    play pause

--- src/App.js
+++ src/App.js
@@ -1,34 +1,57 @@
-import hey from './images/hey.jpg'
-import summer from './images/summer.jpg'
-import ukelee from './images/ukulele.jpg'
-import heyMusic from './music/hey.mp3'
-import summerMusic from './music/summer.mp3'
-import ukuleleMusic from './music/ukulele.mp3'
+import { useRef, useState, useEffect } from 'react'
+import { songsdata } from './data/data';
 
 function App() {
+  // const handleSongPlaying = () => {
+  //   isPlaying === 'music-container' ? setIsPlaying('music-container play') : setIsPlaying('music-container')
+  //   playPauseBtn === "fas fa-play" ? setPlayPauseBtn("fas fa-pause") : setPlayPauseBtn("fas fa-play")
+  //   isPlaying === 'music-container' ? audio.current.play() : audio.current.pause()
+  // }
+  const [songs, setSongs] = useState(songsdata);
+  const [isPlaying, setIsPlaying] = useState(false);
+  const [currentSong, setCurrentSong] = useState(songsdata[0]);
+  const [playing, setplaying] = useState('music-container')
+  const audio = useRef()
+
+  useEffect(() => {
+    if (isPlaying) {
+      audio.current.play()
+    } else {
+      audio.current.pause()
+    }
+  }, [isPlaying])
+  
+
+  //  Play Pause Audio
+  const playPause = () => {
+    setIsPlaying(!isPlaying)
+    playing === 'music-container' ? setplaying('music-container play') : setplaying('music-container')
+  }
   return (
     <div className="App">
       <h1>Music Player</h1>
 
-      <div className="music-container" id="music-container">
+      <div className={playing}>
         <div className="music-info">
-          <h4 id="title"></h4>
+          <h4 id="title">{currentSong.title}</h4>
           <div className="progress-container" id="progress-container">
             <div className="progress" id="progress"></div>
           </div>
         </div>
 
-        <audio src={ukuleleMusic} id="audio"/>
+        <audio src={currentSong.audio} ref={audio}/>
 
         <div className="img-container">
-          <img src={ukelee} alt="music-cover" id="cover" />
+          <img src={currentSong.img} alt="music-cover"/>
         </div>
         <div className="navigation">
           <button id="prev" className="action-btn">
             <i className="fas fa-backward"></i>
           </button>
-          <button id="play" className="action-btn action-btn-big">
-            <i className="fas fa-play"></i>
+          <button id="play" className="action-btn action-btn-big" onClick={playPause}>
+            {
+              isPlaying ? <i className="fas fa-pause"></i> : <i className="fas fa-play"></i>
+            }
           </button>
           <button id="next" className="action-btn">
             <i className="fas fa-forward"></i>
---
+++ src/data/data.js
@@ -0,0 +1,21 @@
+
+export const songsdata = [
+    {
+        id: 1,
+        title: "hey",
+        img: "images/hey.jpg",
+        audio: "music/hey.mp3"
+    },
+    {
+        id: 2,
+        title: "summer",
+        img: "images/summer.jpg",
+        audio: "music/summer.mp3"
+    },
+    {
+        id: 3,
+        title: "ukulele",
+        img: "images/ukulele.jpg",
+        audio: "music/ukulele.mp3"
+    }
+]
